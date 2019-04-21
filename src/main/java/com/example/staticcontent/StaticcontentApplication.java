ca90f45a6fbeac82236c5d08898c6c4b7a9a1006
Pieter-Jan Drouillon <drouillon@hotmail.com>
2019-04-21 09:47:37 +0200

    initial commit

---
+++ src/main/java/com/example/staticcontent/StaticcontentApplication.java
@@ -0,0 +1,13 @@
+package com.example.staticcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticcontentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticcontentApplication.class, args);
+	}
+
+}
