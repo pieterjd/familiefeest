9516e95decef1c92ea154d6df5384c14a6a52032
Pieter-Jan Drouillon <drouillon@hotmail.com>
2019-05-11 18:51:24 +0200

    Added registration component and routing

---
+++ frontend/src/main/angular/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+
+import {RouterModule, Routes} from "@angular/router";
+import {RegistrationComponent} from "./registration/registration.component";
+
+const routes: Routes = [
+  {path:'registration', component: RegistrationComponent},
+
+  ]
+;
+
+
+@NgModule({
+  declarations: [],
+  imports:[
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+
+
+export class AppRoutingModule { }
--- frontend/src/main/angular/src/app/app.module.ts
+++ frontend/src/main/angular/src/app/app.module.ts
@@ -4,20 +4,26 @@ import {NgModule} from '@angular/core';
 // angular material
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 // import widgets
-import {MatCardModule, MatToolbarModule} from "@angular/material";
+import {MatCardModule, MatToolbarModule, MatStepperModule, MatFormFieldModule} from "@angular/material";
 
 import {AppComponent} from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        RegistrationComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
         MatCardModule,
-        MatToolbarModule
+        MatToolbarModule,
+        MatStepperModule,
+        MatFormFieldModule,
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
