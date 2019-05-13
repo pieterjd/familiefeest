6403cf9789b246a5f7446682daf7505539e9aa32
Pieter-Jan Drouillon <drouillon@hotmail.com>
2019-05-13 21:22:30 +0200

    Added eventcode checker component

--- frontend/src/main/angular/src/app/app.module.ts
+++ frontend/src/main/angular/src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule,ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+
 // angular material
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 // import widgets
@@ -11,12 +12,14 @@ import {AppComponent} from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { AppRoutingModule } from './app-routing.module';
 import {EventcodeService} from "./eventcode.service";
+import { EventcodeCheckerComponent } from './eventcode-checker/eventcode-checker.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
-        RegistrationComponent
+        RegistrationComponent,
+        EventcodeCheckerComponent
     ],
     imports: [
         BrowserModule,
