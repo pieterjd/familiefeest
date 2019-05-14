e6029461fb83353f2f5afff7b1e52b1cc51ac445
Pieter-Jan Drouillon <drouillon@hotmail.com>
2019-05-14 21:15:34 +0200

    autoproceed when eventcode is valid

--- frontend/src/main/angular/src/app/registration/registration.component.ts
+++ frontend/src/main/angular/src/app/registration/registration.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {EventcodeService} from "../eventcode.service";
+import {MatHorizontalStepper} from "@angular/material";
 
 @Component({
   selector: 'app-registration',
@@ -10,6 +11,7 @@ import {EventcodeService} from "../eventcode.service";
 export class RegistrationComponent implements OnInit {
   eventCode: any;
   menuFormGroup: FormGroup;
+  @ViewChild(MatHorizontalStepper) stepper: MatHorizontalStepper;
   constructor(private fb: FormBuilder, private eventCodeService: EventcodeService) { }
 
   ngOnInit() {
@@ -22,6 +24,9 @@ export class RegistrationComponent implements OnInit {
 
   setEventCode(eventCode: any){
     this.eventCode = eventCode;
+    if(eventCode != null){
+      this.stepper.next();
+    }
   }
 
 }
