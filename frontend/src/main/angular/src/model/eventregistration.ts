192ef6588df852099ac64b4ecdb97067615d5da3
Pieter-Jan Drouillon <drouillon@hotmail.com>
2019-05-20 19:54:26 +0200

    basic table overview

--- frontend/src/main/angular/src/model/eventregistration.ts
+++ frontend/src/main/angular/src/model/eventregistration.ts
@@ -1,9 +1,13 @@
 import {Event} from "./event";
 import {User} from "./user";
+import {Purchase} from "./purchase";
 
 export class EventRegistration {
   code: string;
   user: User;
   event: Event;
-
+  purchasedItems: Purchase[];
+  purchaseTotal: number;
+  amountAlreadyPaid: number;
+  openAmount: number;
 }
