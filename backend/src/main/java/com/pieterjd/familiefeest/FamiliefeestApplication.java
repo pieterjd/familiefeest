f8d17a72104a70a40c7ae335ac8b10c278c61bb3
Pieter-Jan Drouillon <drouillon@hotmail.com>
2019-05-21 20:48:01 +0200

    clean up

--- backend/src/main/java/com/pieterjd/familiefeest/FamiliefeestApplication.java
+++ backend/src/main/java/com/pieterjd/familiefeest/FamiliefeestApplication.java
@@ -1,13 +1,7 @@
 package com.pieterjd.familiefeest;
 
-import org.h2.command.Command;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootApplication
 public class FamiliefeestApplication {
